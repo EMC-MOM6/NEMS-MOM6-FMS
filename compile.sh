f10dcdf3733e2bbcdd8ceacc5f0c09f57c4bd116
Jessica.Meixner <Jessica.Meixner@noaa.gov>
2018-08-09 14:02:32 +0000

    unifyMOM6cap: pointing to NCARcap branch of MOA-MOM6 at this point code is compiling but having trouble running

--- compile.sh
+++ compile.sh
@@ -63,7 +63,7 @@ if ( ${compile_MOM6_LIB} == 1 ) then
  endif
 
  echo "generating file_paths ..."
- ../../../../src/mkmf/bin/list_paths ../../../../src/MOA-MOM6/src/*/ ../../../../src/MOA-MOM6/src/*/*/ ../../../../src/MOA-MOM6/config_src/dynamic_symmetric/ ../../../../src/MOA-MOM6/config_src/coupled_driver/ 
+ ../../../../src/mkmf/bin/list_paths ../../../../src/MOA-MOM6/src/*/ ../../../../src/MOA-MOM6/src/*/*/ ../../../../src/MOA-MOM6/config_src/dynamic_symmetric/ ../../../../src/MOA-MOM6/config_src/nuopc_driver/{MOM_ocean_model.F90,MOM_surface_forcing.F90}
 
  echo "generating makefile ..."
  ../../../../src/mkmf/bin/mkmf -t ../../../../src/mkmf/templates/${COMPILE_OPTION} -p lib_ocean.a -o '-I../../shared/repro' path_names
